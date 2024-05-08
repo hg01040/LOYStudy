bfbc82459a1cda0ced68cfbed1b33d6fea5cff9d
IMGEONWOO <hg01040@naver.com>
2024-05-09 04:04:46 +0900

    Add files via upload

---
+++ NewNgame.java
@@ -0,0 +1,32 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class NewNgame {
+    public static void main(String[] args) {
+        Random a = new Random();
+        Scanner scanner = new Scanner(System.in);
+        int [] Exam = new int[10];
+        int [] Guess = new int[10];
+        while (true) {
+            int correct = 0;
+            int b = a.nextInt(10, 100);
+            int c = a.nextInt(10, 100);
+            for (int n = 0; n < Exam.length; n++) {
+                Exam [n] = b + c;
+                System.out.println("문제 " + (n + 1));
+                System.out.println("값을 맞춰보세요.");
+                System.out.print(b + " + " + c + " = " + " ? " + "--> ");
+                Guess [n] = scanner.nextInt();
+                if (Exam[n] == Guess[n]) {
+                    correct++;
+                }
+            }
+            System.out.println("성적 " + correct + "점");
+            System.out.println("오답");
+            System.out.println("문제" + (n + 1) + b + " + " + c + " = " + Exam[n]);
+            System.out.print("다시 시작하시겠습니까?(y/n) >> ");
+            if (scanner.next().equals("n")) break;
+        }
+        }
+    }
+
